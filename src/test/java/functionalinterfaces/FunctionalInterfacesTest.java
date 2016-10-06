6068dc4d8a014ab8d801b60c223fdb23aceb4ac5
Anders Back <anders_back@discovery.com>
2016-10-06 17:10:14 +0200

    functional Interfaces

--- src/test/java/functionalinterfaces/FunctionalInterfacesTest.java
+++ src/test/java/functionalinterfaces/FunctionalInterfacesTest.java
@@ -2,12 +2,25 @@ package functionalinterfaces;
 
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class FunctionalInterfacesTest {
 
     @Test
     public void functionalInterfacesTest() {
-        fail("TODO");
+        Function<Integer, String> test = (i) -> i.toString();
+        assertEquals("1", test.apply(1));
+
+        Predicate<String> predicate = (s) -> s.length() > 0;
+        assertTrue(predicate.test("large"));
+
+        Predicate<String> predicate2 = (s) -> "large".equals(s);
+        assertFalse(predicate.and(predicate2).test("lage"));
+        assertTrue(predicate.and(predicate2).test("large"));
     }
 }
