89acd5df8ce6747bd807fe7430a25ef6c9fcb224
Anders Back <anders_back@discovery.com>
2016-10-12 17:14:47 +0200

    lambda local scope

--- src/test/java/lambdadcopes/LambdaScopesTest.java
+++ src/test/java/lambdadcopes/LambdaScopesTest.java
@@ -2,12 +2,22 @@ package lambdadcopes;
 
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
 
 public class LambdaScopesTest {
 
     @Test
-    public void lambdaScopes() {
-        fail("TODO");
+    public void localVariables() {
+        int add = 2;
+        List<Integer> collect = Arrays.asList(1, 2, 3)
+                .stream()
+                .map(o -> o + add)
+                .collect(Collectors.toList());
+
+        assertEquals(Arrays.asList(3, 4, 5), collect);
     }
 }
