b0d1cfcbce3de7f82ad01d6550b81edd7c94c9d0
Anders Back <anders_back@discovery.com>
2016-10-12 17:26:55 +0200

    maps

--- src/test/java/maps/MapsTest.java
+++ src/test/java/maps/MapsTest.java
@@ -2,12 +2,21 @@ package maps;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class MapsTest {
 
     @Test
     public void maps() {
-        fail("TODO");
+        Map<String, String> map = new HashMap<>();
+        map.putIfAbsent("foo", "bar");
+        map.putIfAbsent("foo", "x");
+
+        map.computeIfPresent("foo", (key, val) -> key + val);
+        assertEquals("foobar", map.get("foo"));
     }
 }
