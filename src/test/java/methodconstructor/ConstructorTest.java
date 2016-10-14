685f53865d69bf81b99ef0d9772bd22e8b690794
Anders Back <anders_back@discovery.com>
2016-10-14 17:19:15 +0200

    Added constructor test

--- src/test/java/methodconstructor/ConstructorTest.java
+++ src/test/java/methodconstructor/ConstructorTest.java
@@ -1,12 +1,21 @@
 package methodconstructor;
 
 import org.junit.Test;
+
+import java.util.function.Function;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class ConstructorTest {
 
     @Test
-    public void constructor() {
-        fail("TODO");
+    public void constructorReferences() {
+        Function<byte[], String> f = String::new;
+
+        byte[] data = new byte[1];
+        data[0] = 'a';
+
+        assertEquals("a", f.apply(data));
     }
 }
