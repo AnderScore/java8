81f19962ea8faf4bdd4e0c8b739b95a67bc45d20
Anders Back <anders_back@discovery.com>
2016-10-18 17:16:11 +0200

    parallel streams

--- src/test/java/parallelstreams/ParallelStreamsTest.java
+++ src/test/java/parallelstreams/ParallelStreamsTest.java
@@ -1,6 +1,11 @@
 package parallelstreams;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 import static org.junit.Assert.fail;
 
@@ -8,6 +13,34 @@ public class ParallelStreamsTest {
 
     @Test
     public void parallelStreams() {
-        fail("TODO");
+        int max = 1000000;
+        List<String> values = new ArrayList<>(max);
+        for (int i = 0; i < max; i++) {
+            UUID uuid = UUID.randomUUID();
+            values.add(uuid.toString());
+        }
+
+        //Sequential
+        long start = System.nanoTime();
+
+        values.stream().sorted().count();
+
+        long stop = System.nanoTime();
+
+        long sequentialTime = stop - start;
+        System.out.println("Sequential time:" + sequentialTime);
+
+
+        //Parallel
+        start = System.nanoTime();
+
+        values.parallelStream().sorted().count();
+
+        stop = System.nanoTime();
+
+        long parallelTime = stop - start;
+        System.out.println("Parallel time:" + parallelTime);
+
+        Assert.assertTrue(parallelTime < sequentialTime);
     }
 }
